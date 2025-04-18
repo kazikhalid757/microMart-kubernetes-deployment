6b84914384c256fecd2dc6eace9610604b113999
kazi khalid <kazikhalid757@gmail.com>
2025-04-18 23:58:39 +0600

    update

--- user-service/db.js
+++ user-service/db.js
@@ -2,10 +2,13 @@ const { Pool } = require('pg');
 
 const pool = new Pool({
   host: process.env.DB_HOST,
-  port: process.env.DB_PORT, // Ensure this is defined in the environment
+  port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  ssl: {
+    rejectUnauthorized: false,
+  }
 });
 
 pool.connect()
